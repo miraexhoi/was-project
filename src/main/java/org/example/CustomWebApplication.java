e5223e9360c444f2fc78c47bcd98fc7d08bbb69c
최미래 <miraexhoi@gmail.com>
2024-01-16 21:23:06 +0900

    Feat :: 사용자 요청을 메인 Thread가 처리하도록 구현

--- src/main/java/org/example/CustomWebApplication.java
+++ src/main/java/org/example/CustomWebApplication.java
@@ -3,9 +3,10 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplication {
     private final int port;
@@ -24,6 +25,17 @@ public class CustomWebApplication {
 
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicatioinServer] connected!");
+
+                // 사용자 요청을 메인 Thread가 처리하도록
+                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    String line;
+                    while((line = br.readLine()) != "") {
+                        System.out.println(line);
+                    }
+                }
             }
         }
     }
