2cc54fb30067e43f8e8e9973d582c850d6c52e4f
최미래 <miraexhoi@gmail.com>
2024-01-22 22:31:35 +0900

    Feat :: Create HttpRequest

--- src/main/java/org/example/RequestLine.java
+++ src/main/java/org/example/RequestLine.java
@@ -5,12 +5,12 @@ import java.util.Objects;
 public class RequestLine {
     private final String method; // GET
     private final String urlPath; // /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
-    private String queryString;
+    private QueryStrings queryStrings;
 
     public RequestLine(String method, String urlPath, String queryString) {
         this.method = method;
         this.urlPath = urlPath;
-        this.queryString = queryString;
+        this.queryStrings = new QueryStrings(queryString);
     }
 
     // GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
@@ -27,8 +27,20 @@ public class RequestLine {
 
         // 즉 query 스트링이 있다면
         if (urlPathTokens.length == 2) {
-            this.queryString = urlPathTokens[1];
+            this.queryStrings = new QueryStrings(urlPathTokens[1]);
         }
+    }
+
+    public boolean isGetRequest() {
+        return "GET".equals(this.method);
+    }
+
+    public boolean matchPath(String requestPath) {
+        return urlPath.equals(requestPath);
+    }
+
+    public QueryStrings getQueryStrings() {
+        return this.queryStrings;
     }
 
     @Override
@@ -36,11 +48,11 @@ public class RequestLine {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RequestLine that = (RequestLine) o;
-        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryString, that.queryString);
+        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryStrings, that.queryStrings);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(method, urlPath, queryString);
+        return Objects.hash(method, urlPath, queryStrings);
     }
 }
